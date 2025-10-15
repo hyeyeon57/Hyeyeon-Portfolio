078755a1d480344dba4723ae3c933663d933bfbf
janghaeyo0507-web <janghaeyo0507@gmail.com>
2025-10-16 05:15:42 +0900

    style: Increase spacing between project cards in ?꾩껜 寃쎈줈蹂닿린
    - Change grid gap from gap-8 to gap-12
    - Provides more breathing room between cards
    - Improves visual separation and readability
    - Maintains responsive grid layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -84,7 +84,7 @@ export default function AllProjectsPage() {
         </div>
 
         {/* Projects Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
           {filteredProjects.map((project, index) => (
             <div
               key={project.id}
